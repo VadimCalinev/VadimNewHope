7fdcd701b8b78231189189b1e1efea6177d3fce7
VadimCalinev <57558496+VadimCalinev@users.noreply.github.com>
2020-02-22 14:29:34 +0200

    ClassWork

--- src/homework/data/Cat.java
+++ src/homework/data/Cat.java
@@ -1,80 +1,77 @@
 package data;
 
+import java.io.*;
 
-public class Cat implements FelineInterface, Cloneable{
+public class Cat implements FelineInterface, Cloneable {
+    private String name;
+    private String race;
+    private int year;
+    private Integer id;
 
-  private String name;
-  private String race;
-  private int year;
-  private Integer id;
-
-  public Cat(Integer id, String name, String race, int year) {
-    this.id = id;
-    this.name = name;
-    this.race = race;
-    this.year = year;
-
-  }
-
-
-  @Override
-  public void setByName(String x) {
-
-  }
-
-  @Override
-  public String findByName() {
-    return name;
-  }
-
-  @Override
-  public void setByRace(String x) {
-
-  }
-
-  @Override
-  public String findByRace() {
-    return race;
-  }
-
-  @Override
-  public void setYear(int year) {
-    this.year = year;
-
-  }
-
-  @Override
-  public int getYear() {
-    return year;
-  }
-
-  @Override
-  public void setId(Integer id) {
-    this.id = id;
-
-  }
-
-  @Override
-  public Integer findById() {
-    return id;
-  }
-
-  @Override
-  public String toString() {
-    return "Cat{" +
-            "ID='" + id + '\'' +
-            "name='" + name + '\'' +
-            ", race='" + race + '\'' +
-            ", year=" + year +
-            '}';
-  }
-
-  public Cat clone() {
-    try {
-      return (Cat) super.clone();
-    } catch (CloneNotSupportedException e) {
-      return new Cat(this.id, this.name, this.race, this.year);
+    public Cat(Integer id, String name, String race, int year) {
+        this.id = id;
+        this.name = name;
+        this.race = race;
+        this.year = year;
     }
 
-  }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getRace() {
+        return race;
+    }
+
+    @Override
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    @Override
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    
+
+	@Override
+	public FelineInterface  clone() throws CloneNotSupportedException {
+		
+		return (FelineInterface)super.clone();
+	}
+
+	@Override
+    public String toString() {
+        return "Cat{" +
+                "ID='" + id + '\'' +
+                "name='" + name + '\'' +
+                ", race='" + race + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
 }
