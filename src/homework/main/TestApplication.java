1d424ac63b34cfb5a2499db7629ba97e849c2077
VadimCalinev <57558496+VadimCalinev@users.noreply.github.com>
2020-02-22 14:30:32 +0200

    Class Work

--- src/homework/main/TestApplication.java
+++ src/homework/main/TestApplication.java
@@ -1,42 +1,32 @@
 package main;
 
-
 import data.Cat;
 import data.FelineInterface;
-import persistance.FelineRepository;
-import persistance.FelineRepositoryInterface;
+import persistence.FelineRepository;
 
-import java.util.ArrayList;
+public class TestApplication {
 
-
-public class TestApplication implements Cloneable{
-
-    public static void main(String[] args) throws CloneNotSupportedException {
-
-        FelineRepositoryInterface fr = new FelineRepository();
-        // ^^^^^^^^^^^^ | ^^ | ^^^^^^^
-        // Тип переменной | Название |
-
-        ((FelineRepository) fr).generate();
-
-        // System.out.println(fr.get(11));
-
-        FelineInterface cat = fr.findById(22);
-        cat.setByName("Masya");
-
-
-
-        for (FelineInterface f : fr.findAll()) {
-            System.out.println(f);
-        }
-
-        // getByName(String name) -> List<FelineInterface>
-        // getByRace(String race) -> List<FelineInterface>
-        Cat cat1 = (Cat) fr.findById(1);
-        Cat cat2 = (Cat) cat1.clone();
-        cat2.setByName("another name");
-        System.out.println(cat1);
-        System.out.println(cat2);
-    }
-
+	public static void main(String[] args) throws CloneNotSupportedException{
+		
+		//FelineInterface cat = new Cat(1, "Murzik", "British", 2018);
+		//FelineInterface cat2 = new Cat(2, "Murzik", "Britishe", 2018);
+		//FelineInterface clonned = cat.clone();
+		//FelineInterface clonned2 = cat2.clone();
+		//FelineRepository StupidReposirtory = new FelineRepository();
+		//StupidReposirtory.generate();
+		//System.out.println(StupidReposirtory.findByName("Murzik"));
+		//System.out.println(StupidReposirtory.findByRace("Britishe"));
+		
+		FelineRepository fr = new FelineRepository();
+		fr.generate();
+		FelineInterface original = fr.findById(22);
+		original.setYear(1900);
+		original.setId(10000);
+		fr.save(original);
+		System.out.println( fr.findAll());
+		
+		
+		
+	}
 }
+
