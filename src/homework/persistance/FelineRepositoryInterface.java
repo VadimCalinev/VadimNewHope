4d20c76d0d3aa699bb5ef0f0262397fb6ae575e6
VadimCalinev <57558496+VadimCalinev@users.noreply.github.com>
2020-02-22 14:31:48 +0200

    Class Work

--- src/homework/persistance/FelineRepositoryInterface.java
+++ src/homework/persistance/FelineRepositoryInterface.java
@@ -1,24 +1,21 @@
-package persistance;
-
-import java.util.List;
+package persistence;
 
 import data.FelineInterface;
 
+import java.util.List;
+
 public interface FelineRepositoryInterface {
+    boolean save(FelineInterface feline);
 
-    public boolean save(FelineInterface feline);
+    boolean delete(FelineInterface feline);
 
-    public boolean delete(FelineInterface feline); //
+    boolean update(FelineInterface feline); // Сначала находим этот объект, потом перезаписываем
 
-    public boolean update(FelineInterface feline); // Сначала находим этот объект, потом перезаписываем
+    List<FelineInterface> findAll() throws CloneNotSupportedException; // Будет возвращать Empty List, если объекта не найдено
 
-    public FelineInterface findByName(String name);
+    FelineInterface findById(Integer id)throws CloneNotSupportedException; // Будет возвращать null, если объекта не найдено
 
-    public FelineInterface findByRace(String race);
+    List<FelineInterface> findByName(String name)throws CloneNotSupportedException;
 
-
-    public List<FelineInterface> findAll(); // Будет возвращать Empty List, если объекта не найдено
-
-    public FelineInterface findById(Integer id); // Будет возвращать null, если объекта не найдено
-
+    List<FelineInterface> findByRace(String race)throws CloneNotSupportedException;
 }
